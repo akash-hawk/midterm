f1fe572226dbe95a52997b7e4536d5021d6e326e
Akash Goindi <41852582+AkashGoindi@users.noreply.github.com>
2024-04-11 16:09:00 -0500

    CORS error removed

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,17 @@ import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { prismaClient } from "./lib/db";
+import cors from "cors"; 
 
 async function init() {
   const app = express();
   const PORT = Number(process.env.PORT) || 3000;
   app.use(express.json());
+
+  app.use(cors());
+  // app.use(cors({
+  //   origin: 'https://yourfrontenddomain.com'
+  // }));
 
   const gqlserver = new ApolloServer({
     typeDefs: `
